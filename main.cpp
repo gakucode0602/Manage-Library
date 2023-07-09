2f16d87d5d0158f46f32d57758685109e5bebf7b
gakucode0602 <123054639+gakucode0602@users.noreply.github.com>
2023-07-09 22:51:06 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include "book_info_copy.h"
+#include "reader_info_copy.h"
+#include<iostream>
+#include<vector>
+#include<cstdlib>
+#include<ctime>
+#include<algorithm>
+#include<string>
+using namespace std;
+int main()
+{
+    return 0;
+}
